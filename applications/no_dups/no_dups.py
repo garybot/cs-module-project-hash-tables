93d629fcc875b5d4a5a1da01f75f80c367493e05
garybot <argot.pryer@gmail.com>
2020-09-10 17:59:22 -0700

    I think this is a good solution!

--- applications/no_dups/no_dups.py
+++ applications/no_dups/no_dups.py
@@ -1,7 +1,28 @@
+# def no_dups(s):
+#     arr = set(s.split())
+#     return len(" ".join(arr))
+
 def no_dups(s):
-    # Your code here
+	new_string = ""
+	word = ""
+	words = {}
 
+	for char in s: #n
+		if char != " ": #1
+			word += char #1
+		else:
+			if not words.get(word): #1
+				if len(new_string): #1
+					new_string += " " #1
+				words[word] = True #1
+				new_string += word #1
 
+			word = "" #1
+
+	if not words.get(word): #1
+		new_string += word #1 
+
+	return len(new_string)
 
 if __name__ == "__main__":
     print(no_dups(""))
